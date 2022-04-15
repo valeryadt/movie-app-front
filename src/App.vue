1d3a8ee891d4677063305e93a92fcaece7d91e2c
Валерия Дашдамирова <valeriadasdamirova@MacBook-Pro-Valeria.local>
2022-04-15 18:36:04 +0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <ApiTest />
+    <h1>lalalala</h1>
+    <hr>
+  </div>
+</template>
+
+<script>
+import ApiTest from "@/components/ApiTest";
+
+export default {
+  name: 'App',
+  components: {
+    ApiTest
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ApiTest.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+  <h1>{{message}}</h1>
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: "ApiTest",
+  data() {
+    return {
+      message: ''
+    }
+  },
+  mounted() {
+    axios
+        .get('http://277809.simplecloud.ru/health')
+        .then((res) => {
+          this.message = res.data.response;
+        })
+        .catch((err) => {
+          this.error = true;
+        })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
